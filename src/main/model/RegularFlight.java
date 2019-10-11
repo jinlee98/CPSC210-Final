85c159e57884b9b3eb260ad2e0b3854ffbc7243a
Jin <a3i2b@ugrad.cs.ubc.ca>
2019-10-10 19:53:32 -0700

    commit6

--- src/main/model/RegularFlight.java
+++ src/main/model/RegularFlight.java
@@ -4,12 +4,12 @@ public class RegularFlight extends Airport {
 
     @Override
     public boolean makeNewDeparture(Plane c, int departureTime) {
-        if (departureTime >= departuresList.size()) {
+        if (departureTime >= departures.size()) {
             System.out.println("That departure time is not available.");
             return true;
         }
         System.out.println("Flight " + c.getName() + " is departing at at " + departureTime);
-        departuresList.set(departureTime, c);
+        departures.set(departureTime, c);
         c.setDepartureTime(departureTime);
         return true;
     }
--- src/main/model/UrgentFlight.java
+++ src/main/model/UrgentFlight.java
@@ -5,7 +5,7 @@ public class UrgentFlight extends Airport {
     @Override
     public boolean makeNewDeparture(Plane c, int departureTime) {
         System.out.println("Flight " + c.getName() + " is departing at at " + departureTime);
-        departuresList.set(departureTime, c);
+        departures.set(departureTime, c);
         c.setDepartureTime(departureTime);
         return true;
     }
